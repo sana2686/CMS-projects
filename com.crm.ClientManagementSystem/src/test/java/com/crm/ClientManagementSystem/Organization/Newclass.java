0f51671903044d1b1f3040d00c2d21eaa3d5a0d8
admin <admin@DESKTOP-5U0IUST>
2026-04-20 13:39:08 +0530

    sana secound commit

---
+++ com.crm.ClientManagementSystem/src/test/java/com/crm/ClientManagementSystem/Organization/Newclass.java
@@ -0,0 +1,14 @@
+package com.crm.ClientManagementSystem.Organization;
+
+import org.testng.annotations.Test;
+
+import com.crm.ClientManagementSystem.GenericLibrary.BaseClass;
+
+public class Newclass extends BaseClass{
+	
+	@Test
+	public void modify() {
+		
+	}
+
+}
