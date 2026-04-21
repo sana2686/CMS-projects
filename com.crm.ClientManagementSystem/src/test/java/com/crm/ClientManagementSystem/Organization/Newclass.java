09302361ea70591e0823b1cc1d04ba4fd1348d40
admin <admin@DESKTOP-5U0IUST>
2026-04-21 07:32:14 +0530

    sana third commit

--- com.crm.ClientManagementSystem/src/test/java/com/crm/ClientManagementSystem/Organization/Newclass.java
+++ com.crm.ClientManagementSystem/src/test/java/com/crm/ClientManagementSystem/Organization/Newclass.java
@@ -1,5 +1,6 @@
 package com.crm.ClientManagementSystem.Organization;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.crm.ClientManagementSystem.GenericLibrary.BaseClass;
@@ -8,6 +9,7 @@ public class Newclass extends BaseClass{
 	
 	@Test
 	public void modify() {
+		Reporter.log("report genarate",true);
 		
 	}
 
