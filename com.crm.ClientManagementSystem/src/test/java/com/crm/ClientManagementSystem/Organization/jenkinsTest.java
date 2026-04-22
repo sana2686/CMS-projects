351d55dbdc9b24761ff5307ac12bca83ba2030f7
admin <admin@DESKTOP-5U0IUST>
2026-04-22 16:51:11 +0530

    sana fifyh commit

---
+++ com.crm.ClientManagementSystem/src/test/java/com/crm/ClientManagementSystem/Organization/jenkinsTest.java
@@ -0,0 +1,13 @@
+package com.crm.ClientManagementSystem.Organization;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class jenkinsTest {
+	@Test
+	public  void jenkis_class () {
+		Reporter.log("jenkins is executed by git",true);
+		
+	}
+
+}
